1ead9944573319e156f7fced9cc9873267aa82f9
Javier Hernández <jhernandez@emergya.com>
2013-08-12 13:35:57 +0200

    Adding device reporter stub
      - Adding the initial stub
      - Adding required bits to create the stub into makestubs.sh

---
+++ platform/devicereporter/src/org/meshpoint/anode/stub/gen/platform/Net_gpii_AndroidDeviceReporter.java
@@ -0,0 +1,15 @@
+/* This file has been automatically generated; do not edit */
+
+package org.meshpoint.anode.stub.gen.platform;
+
+public final class Net_gpii_AndroidDeviceReporter {
+
+	private static Object[] __args = new Object[0];
+
+	public static Object[] __getArgs() { return __args; }
+
+	static Object __invoke(net.gpii.AndroidDeviceReporter inst, int opIdx, Object[] args) {
+		return inst.getJSONOutput();
+	}
+
+}
