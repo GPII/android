525960c83937a99c1f09246bd7f7f317c0e7d7a6
Javier Hernández <jhernandez@emergya.com>
2013-10-26 18:25:52 +0200

    GPII-277: Adding get/set wrapper for Android.Settings
    [Android.Settings wrapper]
      * These get/set methods provides us to get/set values from Android's
    native Android.Settings. This wrappers can deal with settings from
    Settings.System, Settings.Secure and Settings.Global
    
    [More changes]
      * Move current AndroidFontSettings into a separate folder called
    'AndroidFontSettings'
      * Add the new code into a folder called 'AndroidSettings'

---
+++ platform/nativesettings/src/net/gpii/AndroidSettings/AndroidSettingsImpl.java
@@ -0,0 +1,71 @@
+package net.gpii;
+
+import org.meshpoint.anode.AndroidContext;
+
+import android.util.Log;
+import android.content.Context;
+import android.provider.Settings;
+
+import org.meshpoint.anode.module.IModule;
+import org.meshpoint.anode.module.IModuleContext;
+
+public class AndroidSettingsImpl extends AndroidSettings implements IModule {
+    private static final String TAG = "net.gpii.AndroidSettingsImpl";
+
+    IModuleContext ctx;
+    private Context androidContext;
+
+    @Override
+    public Object startModule(IModuleContext ctx) {
+        Log.v(TAG, "AndroidSettingsImpl.startModule");
+        try {
+            this.ctx = ctx;
+            androidContext = ((AndroidContext) ctx).getAndroidContext();
+        }
+        catch (Exception e) {
+            Log.v(TAG, "AndroidSettingsImpl error starting module: " + e);
+        }
+        return this;
+    }
+
+    @Override
+    public void stopModule() {
+        Log.v(TAG, "AndroidSettingsImpl.stopModule");
+    }
+
+    @Override
+    public String get(String settingType, String setting) {
+        Log.v(TAG, "AndroidSettingsImpl.get: " + setting);
+
+        String value = null;
+        if (settingType.contains("Secure")) {
+            value = Settings.Secure.getString(androidContext.getContentResolver(), setting);
+        } else if (settingType.contains("System")) {
+            value = Settings.System.getString(androidContext.getContentResolver(), setting);
+        } else if (settingType.contains("Global")) {
+            value = Settings.Global.getString(androidContext.getContentResolver(), setting);
+        } else {
+            Log.e(TAG, "We haven't implemented this type yet: " + settingType);
+        }
+
+        return value;
+    }
+
+    @Override
+    public Boolean set(String settingType, String setting, String value) {
+        Log.v(TAG, "AndroidSettingsImpl.set: " + setting + " to " + value);
+
+        Boolean result = false;
+        if (settingType.contains("Secure")) {
+            result = Settings.Secure.putString(androidContext.getContentResolver(), setting, value);
+        } else if (settingType.contains("System")) {
+            result = Settings.System.putString(androidContext.getContentResolver(), setting, value);
+        } else if (settingType.contains("Global")) {
+            result = Settings.Global.putString(androidContext.getContentResolver(), setting, value);
+        } else {
+            Log.e(TAG, "We haven't implemented this type yet: " + settingType);
+        }
+
+        return result;
+    }
+}
