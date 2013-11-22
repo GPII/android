9763fc1d48147b18ac7d1935424836e99e7ab5b3
Javier Hernández <jhernandez@emergya.com>
2013-11-22 16:27:01 +0100

    GPII-277: Renaming nativesettings into androidSettings
    * Rename module from nativesettings into androidSettings
    * Moving back the java code to the original folder
    * Updating ant.properties and makestubs.sh according to these changes

--- platform/makestubs.sh
+++ platform/makestubs.sh
@@ -2,5 +2,5 @@
 
 java -jar ./anode/sdk/java/tools/stubgen.jar --verbose --out ./intents/src --classpath ./app/bin/classes net.gpii.AndroidIntentHandler
 java -jar ./anode/sdk/java/tools/stubgen.jar --verbose --out ./a11yservices/src --classpath ./app/bin/classes net.gpii.AndroidA11ySettings
-java -jar ./anode/sdk/java/tools/stubgen.jar --verbose --out ./nativesettings/src --classpath ./app/bin/classes net.gpii.AndroidFontSettings
-java -jar ./anode/sdk/java/tools/stubgen.jar --verbose --out ./nativesettings/src --classpath ./app/bin/classes net.gpii.AndroidSettings
+java -jar ./anode/sdk/java/tools/stubgen.jar --verbose --out ./androidSettings/src --classpath ./app/bin/classes net.gpii.AndroidFontSettings
+java -jar ./anode/sdk/java/tools/stubgen.jar --verbose --out ./androidSettings/src --classpath ./app/bin/classes net.gpii.AndroidSettings
