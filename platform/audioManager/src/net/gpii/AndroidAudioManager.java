2a707655405cbbd09862fcb05aeecb9141ba3df7
Javier Hernández <jhernandez@emergya.com>
2013-12-10 17:14:33 +0100

    GPII-398: Re-indent with spaces instead of tabs

--- platform/audioManager/src/net/gpii/AndroidAudioManager.java
+++ platform/audioManager/src/net/gpii/AndroidAudioManager.java
@@ -4,9 +4,9 @@ import org.meshpoint.anode.bridge.Env;
 import org.meshpoint.anode.java.Base;
 
 public abstract class AndroidAudioManager extends Base {
-	private static short classId = Env.getInterfaceId(AndroidAudioManager.class);
-        public AndroidAudioManager() { super(classId); }
-	
-	public abstract Integer getVolume(String setting);
-	public abstract void setVolume(String setting, Integer value);
+    private static short classId = Env.getInterfaceId(AndroidAudioManager.class);
+    public AndroidAudioManager() { super(classId); }
+    
+    public abstract Integer getVolume(String setting);
+    public abstract void setVolume(String setting, Integer value);
 }
