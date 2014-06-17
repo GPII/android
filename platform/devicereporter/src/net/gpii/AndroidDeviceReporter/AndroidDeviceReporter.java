77752ede51cb02f4231636e6b8448b43068b2701
Javier Hernández <jhernandez@emergya.com>
2014-06-17 11:19:03 +0200

    GPII-779: Updating AndroidDeviceReporter Java code
      - Create a useful JSON output
      - Disable some parts of the device reporting
      - Code clean up

--- platform/devicereporter/src/net/gpii/AndroidDeviceReporter/AndroidDeviceReporter.java
+++ platform/devicereporter/src/net/gpii/AndroidDeviceReporter/AndroidDeviceReporter.java
@@ -4,8 +4,7 @@ import org.meshpoint.anode.bridge.Env;
 import org.meshpoint.anode.java.Base;
 
 public abstract class AndroidDeviceReporter extends Base {
-	private static short classId = Env.getInterfaceId(AndroidDeviceReporter.class
-);
+	private static short classId = Env.getInterfaceId(AndroidDeviceReporter.class);
 	public AndroidDeviceReporter() { super(classId); }
         public abstract String getJSONOutput();
 }
