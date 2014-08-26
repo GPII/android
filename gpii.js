36222799e6ef301796eef590089d3e6732c5e764
Javier Hernández <jhernandez@emergya.com>
2014-08-26 16:40:31 +0200

    GPII-900: Linting all javascript source code
    * Updating a few copyrights

--- gpii.js
+++ gpii.js
@@ -2,6 +2,7 @@
 GPII Linux Personalization Framework Node.js Bootstrap
 
 Copyright 2012 OCAD University
+Copyright 2013, 2014 Emergya
 
 Licensed under the New BSD license. You may not use this file except in
 compliance with this License.
